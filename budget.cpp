c34656041b116edae48ff73fa66b439d9af06496
ketillkline <jrod4601@gmail.com>
2025-11-13 15:30:37 -0500

    Refactor budget.cpp for improved readability

--- budget.cpp
+++ budget.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
 #include "budget.h"
+#include <iostream>
 
 Budget::Budget(int starting_amount){
     amount = starting_amount;
-    
-    
 };
 
 void Budget::add_money(int value){
-    amount += value;    
+    amount+=value;
 };
 
 void Budget::use_money(int value){
+    if (value > amount){
+        std::cout << "ERROR: Insufficient Funds." << std::endl;
+        return;
+    }
     amount -= value;
 };
 
 void Budget::display_money() const{
-    std::cout << "You have $" << amount << " remaining." << std::endl;  
+  std::cout << "Current Amount: " << amount << std::endl;  
 };
